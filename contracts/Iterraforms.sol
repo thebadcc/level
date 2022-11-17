efc253a13b103c71b91558a080152f95a945da07
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-11-17 17:23:26 -0500

    Update Iterraforms.sol

--- contracts/Iterraforms.sol
+++ contracts/Iterraforms.sol
@@ -2,8 +2,20 @@
 pragma solidity ^0.8.0;
 
 interface Iterraforms {
-  function getStatus() public view returns (uint);
-  function getSeed() public view returns (uint);
-  function getPlacement() public view returns (uint);  
+    
+    function seed(uint) 
+        external 
+        view 
+        returns (uint);
+  
+    function tokenToPlacement(uint) 
+        external 
+        view 
+        returns (uint);
+  
+    function tokenToStatus(uint) 
+        external 
+        view 
+        returns (uint);  
 }
 
