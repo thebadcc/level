5c43d1e8c46c76647e047cd1f287e3f256bd3b36
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-11-17 10:54:45 -0500

    Update and rename falseIdosInterface.sol to Iterraforms.sol

---
+++ contracts/Iterraforms.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface Iterraforms {
+  function getStatus() public view returns (uint);
+  function getSeed() public view returns (uint);
+  function getPlacement() public view returns (uint);  
+}
+
--- contracts/falseIdosInterface.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.7.0) (token/ERC721/ERC721.sol)
-
-pragma solidity ^0.8.0;
-// contract interface
-contract UsersAgesInterface {
-  // function definition of the method we want to interact with
-  function getAge(address _myAddress) public view returns (uint);
-}
