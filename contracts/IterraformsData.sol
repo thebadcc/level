daac3cf19478ae64f6dbe5bfccd80270394d2cb5
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-11-17 17:18:09 -0500

    Update IterraformsData.sol

--- contracts/IterraformsData.sol
+++ contracts/IterraformsData.sol
@@ -2,5 +2,8 @@
 pragma solidity ^0.8.0;
 
 interface IterraformsData {
-  function getTokenSVG(uint indexed status, uint indexed placement, uint indexed seed, uint indexed decay, uint256[] indexed canvas) public view returns (string);
+    function tokenSVG(uint, uint, uint, uint, uint[] memory) 
+        external 
+        view 
+        returns (string memory);
 }
