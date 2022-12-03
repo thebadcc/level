f2ab502de38a229172954bd9b66f6b514aa5b614
thebadcc <95499220+thebadcc@users.noreply.github.com>
2022-12-02 21:17:39 -0500

    Delete IterraformsData.sol

--- contracts/IterraformsData.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface IterraformsData {
-    function tokenSVG(uint, uint, uint, uint, uint[] memory) 
-        external 
-        view 
-        returns (string memory);
-}
